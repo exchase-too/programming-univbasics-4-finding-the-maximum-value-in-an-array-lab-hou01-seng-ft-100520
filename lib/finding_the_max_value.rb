cc98cb4ac4b602297747a6be115a744b115c630f
Eric Smith <eric.c.smith@sbcglobal.net>
2020-09-15 18:25:32 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,4 +1,11 @@
 def find_max_value(array)
   count = 0 
+  max = 0 
+  while count < array.size do
+    if array[count] > max 
+      max = array[count]
+    end
+    count += 1 
+  end
   max
 end
